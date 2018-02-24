4274c63528b05685b1f84636f3c700bc78863b41
Youngmi Lee <abinding@gmail.com>
2018-02-24 09:57:50 -0600

    second commit

---
+++ assets/css/style.css
@@ -0,0 +1,38 @@
+* {
+    box-sizing: border-box;
+  }
+  
+  html {
+    height: 100%
+  }
+  
+  body {
+    background-image: url("https://screenshots.macupdate.com/JPG/36909/36909_1500034826_scr.jpg");
+    background-size: cover;
+    background-image-opacity: 0.3;
+  }
+  
+  #mainBody {
+    height: 100%;
+    width: 50%;
+    background: #fff;
+    margin: 500px auto;
+    padding: 20px;
+    opacity: 0.7;
+    text-align: center;
+  }
+  
+  table {
+   margin: 0 auto; 
+  }
+  
+  table tr td {
+    border: 2px solid #444;
+  }
+  
+  a {
+    display: inline-block;
+    margin-bottom: 20px;
+    color: #000;
+    font-family: Arial, sans-serif;
+  }
